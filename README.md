a7ffc05b5b08617ba09a78ac7adbd7f36d9421e6
ram0973 <ram0973@gmail.com>
2016-11-23 19:38:18 +0300

    mass fixes

--- README.md
+++ README.md
@@ -1,98 +1,29 @@
-# Решение задачи [№10](https://devman.org/challenges/10/) с сайта [devman.org](https://devman.org)
-
-## Условие задачи:
-
-В этой задаче требуется собрать информацию о разных курсах на Курсере, 
-привести её в удобный для обработки вид и выгрузить в эксель-файл.
-
-Порядок действий такой:
-
-Вытащить список курсов из [xml-фида](https://www.coursera.org/sitemap~www~courses.xml) 
-Курсеры, хотя бы случайные 20. Для парсинга xml подойдёт, например, lxml.
-Зайти на страницу курса и вытащить оттуда название, язык, 
-ближайшую дату начала, количество недель и среднюю оценку. 
-Для получения данных хорошо использовать requests, а для парсинга - beautifulsoup4.
-Выгрузить эти данные в xlsx-файл, один курс – одна строка. 
-Для работы с эксель-файлами можно использовать openpyxl.
-Задача довольно большая и что-то может быть совсем непонятно, 
-не работать, не устанавливаться, быть недокументированным и сырым. 
-Это нормально. Борись с этим.
-
-
-## Системные требования
-
+# Task [№10](https://devman.org/challenges/10/) from [devman](https://devman.org)
+## Requirements
 ```
 Python 3.5.2+
-Внешний модуль requests
-Внешний модуль win-unicode-console
-Внешний модуль lxml
-Внешний модуль beautifulsoup4
-Внешний модуль openpyxl
-Внешний модуль babel
+requests
+lxml
+openpyxl
+babel
+tqdm
 ```
-
-## Установка
-
-Windows
-
-Пакет lxml может не установиться, качаем его wheel 
-[отсюда](http://www.lfd.uci.edu/~gohlke/pythonlibs/#lxml)
-и ставим нужную библиотеку под вашу версию питона вручную,
-пример - pip install lxml-3.6.4-cp35-cp35m-win32.whl
-
-```    
-git clone https://github.com/ram0973/10_coursera.git
-cd 10_coursera
-pip install -r requirements.txt
-```
-
-Linux
+## Setup
 ```    
 git clone https://github.com/ram0973/10_coursera.git
 cd 10_coursera
 pip3 install -r requirements.txt
 ```
-    
-    
-## Описание работы
-Пользователь вводит два необязательных аргумента - 
-1) mode - способ получения данных - через сайт или через api 
-(по умолчанию через сайт),
-2) count - число курсов для вывода (по умолчанию 20)
-И один обязательный - outfile - имя файла с результатом
-
-Скрипт загружает url курсов из XML-файла COURSERA_COURSES_XML,
-полученные url курсов перемешиваются случайным образом, 
-отбирается COURSES_COUNT курсов.
-
-Далее, зависимости от выбранного способа загрузки информации:
-
-Страницы курсов парсятся, или скрипт получает данные из 
-[api](https://building.coursera.org/app-platform/catalog/)   
-
-Далее полученные данные выводятся в файл OUTPUT_XSLX_FILENAME.
-
-## Настройки
-
-```
-COURSERA_COURSES_XML - путь к xml-файлу 
-COURSES_COUNT - количество курсы для вывода в файл по умолчанию
-```
-
-## Запуск
-
-Windows
-
-```
-python coursera.py --o result.xlsx
-```
- 
-Linux
-
+## Description
+The user enters an optional argument count - the number of courses to display.
+And one mandatory - outfile - the file name with the result
+The script loads the urls of courses, courses received urls randomly shuffled,
+Next, the script retrieves data from
+[Api](https://building.coursera.org/app-platform/catalog/)
+The obtained data saves in file.
+## Usage
 ``` 
 python3 coursera.py --o result.xlsx
 ```
- 
-## Лицензия
-
+## License
 [MIT](http://opensource.org/licenses/MIT)
--- coursera.py
+++ coursera.py
@@ -1,143 +1,38 @@
 # -*- coding: utf-8 -*-
 import argparse
-import json
 import os
 import random
-import re
 import requests
-import sys
 from lxml import etree
-from bs4 import BeautifulSoup
 from tqdm import tqdm
 import time
 import babel
 from openpyxl import Workbook
 
-CONNECT_TIMEOUT = 9
-READ_TIMEOUT = 9
 COURSERA_COURSES_XML = 'https://www.coursera.org/sitemap~www~courses.xml'
 COURSERA_API_URL = 'https://api.coursera.org/api/courses.v1'
 COURSES_COUNT = 20
 
 
-def enable_win_unicode_console():
-    """
-    Включаем правильное отображение unicode в консоли под MS Windows
-    """
-    if sys.platform == 'win32':
-        import win_unicode_console
-        win_unicode_console.enable()
-
-
-def handle_requests_library_exceptions(decorated):
-    """
-    Декоратор, обрабатывающий ошибки в requests
-    :param decorated: Функция, в которой надо отловить requests exceptions
-    :return: декоратор
-    """
-    def decorator(*args, **kwargs):
-        try:
-            return decorated(*args, **kwargs)
-        except requests.ConnectionError:
-            print('Ошибка сетевого соединения')
-            exit(1)
-        except requests.HTTPError as e:
-            print('Сервер вернул неудачный код статуса ответа: %s %i' %
-                  (e.response.reason, e.response.status_code))
-            exit(1)
-        except requests.Timeout:
-            print('Вышло время ожидания ответа от сервера')
-            exit(1)
-        except requests.TooManyRedirects:
-            print('Слишком много редиректов')
-            exit(1)
-
-    return decorator
-
-
-@handle_requests_library_exceptions
-def get_courses_list(url: str) -> list:
-    """
-    Получаем список url курсов с сервера
-    :param url: url XML файла
-    :return: список всех url курсов
-    """
+def get_courses_list(url):
     response = requests.get(url)
-    response.raise_for_status()
     tree = etree.XML(response.content)
     return [element[0].text for element in tree]
 
 
-def get_course_info(course_slug: str) -> list:
-    """
-    Получаем данные по курсу с его веб-страницы
-    :param course_slug: url курса
-    :return: список данных по курсу
-    """
-    try:
-        response = requests.get(course_slug, timeout=(
-            CONNECT_TIMEOUT, READ_TIMEOUT))
-        response.raise_for_status()
-    except (requests.ConnectionError, requests.HTTPError, requests.Timeout,
-            requests.TooManyRedirects):
-        return [None, None, None, None, None]
-
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-    title = soup.find('div', class_='title').text
-    title = title if title else None
-
-    lang = soup.find('div', class_='language-info').text
-    lang = lang if lang else None
-
-    try:
-        course_json = soup.select('script[type="application/ld+json"]')[0]
-        course_info = json.loads(course_json.text) if course_json else None
-        start_date = course_info['hasCourseInstance'][0]['startDate']
-    except (KeyError, IndexError):
-        start_date = None
-
-    weeks = len(soup.find_all('div', class_='week'))
-    weeks = weeks if weeks else None
-
-    ratings = soup.find('div', class_='ratings-text')
-    ratings = re.search('\d\.\d', ratings.text) if ratings else None
-    ratings = float(ratings.group()) if ratings else None
-
-    return [title, lang, start_date, weeks, ratings]
-
-
-def get_course_info_from_api(course_slug: str) -> list:
-    """
-    Получаем данные по курсу из api
-    https://building.coursera.org/app-platform/catalog/
-    :param course_slug: url курса
-    :return: список данных по курсу
-    """
+def get_course_info(course_slug):
     slug = course_slug.split('/')[-1]
     url = COURSERA_API_URL
-    params = {'q': 'slug', 'slug': slug, 'includes': 'courseDerivatives',
-              'fields': 'name,primaryLanguages,subtitleLanguages,' +
-                        'courseDerivatives.v1(averageFiveStarRating),' +
-                        'startDate,workload'}
-    try:
-        response = requests.get(url, timeout=(
-            CONNECT_TIMEOUT, READ_TIMEOUT), params=params)
-        response.raise_for_status()
-    except (requests.ConnectionError, requests.HTTPError, requests.Timeout,
-            requests.TooManyRedirects):
-        return [None, None, None, None, None]
-
-    course = response.json()
-
+    payload = {'q': 'slug', 'slug': slug, 'includes': 'courseDerivatives',
+               'fields': 'name,primaryLanguages,subtitleLanguages,' +
+                         'courseDerivatives.v1(averageFiveStarRating),' +
+                         'startDate,workload'}
+    course = requests.get(url, params=payload).json()
     title = course['elements'][0]['name']
-
     lang = course['elements'][0]['primaryLanguages'] + \
-        course['elements'][0]['subtitleLanguages']
-    lang = ', '.join(
-        list(map(lambda x: babel.Locale.parse(x, sep='-').display_name, lang)))
-
-    # coursera api дает epoch time в миллисекундах, или строку с описанием даты
+           course['elements'][0]['subtitleLanguages']
+    lang = ', '.join(list(map(
+        lambda x: babel.Locale.parse(x, sep='-').display_name, lang)))
     start_date = course['elements'][0]['startDate']
     if start_date:
         try:
@@ -145,24 +40,16 @@ def get_course_info_from_api(course_slug: str) -> list:
             start_date = time.strftime('%Y-%m-%d ', time.localtime(start_date))
         except ValueError:
             pass
-
     weeks = course['elements'][0]['workload']
-
     try:
         ratings = course['linked']['courseDerivatives.v1'][0]
         ratings = ratings['averageFiveStarRating']
     except KeyError:
         ratings = None
-
     return [title, lang, start_date, weeks, ratings]
 
 
-def output_courses_info_to_xlsx(courses: list, filepath: str):
-    """
-    Вывводим данные по курсам в файл
-    :param courses: список данных по курсам
-    :param filepath: путь к файлу
-    """
+def output_courses_info_to_xlsx(courses, filepath):
     wb = Workbook()
     ws = wb.active
     for course in courses:
@@ -171,58 +58,25 @@ def output_courses_info_to_xlsx(courses: list, filepath: str):
 
 
 def set_file_extension(file_name: str, new_ext: str):
-    """ Меняем расширение файла """
     return '{}.{}'.format(os.path.splitext(file_name)[0], new_ext)
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--mode', '--m',
-                        help='Введите "api" или "html" без кавычек',
-                        choices=['api', 'html'],
-                        default='api')
     parser.add_argument('--count', '--c',
-                        help='Введите количество курсов',
-                        default=COURSES_COUNT,
-                        type=int)
-    parser.add_argument('--outfile', '--o', help='Путь к файлу с результатами',
-                        # required=True
-                        )
-
-    output_file = parser.parse_args().outfile
-    if not output_file:
-        print('Ошибка: вы не задали путь к файлу .xlsx')
-        parser.print_usage()
-        exit(1)
-    output_file = set_file_extension(output_file, 'xlsx')
-
-    # тут проверка, целое ли это число будет в argparse
+                        help='Courses count', default=COURSES_COUNT, type=int)
+    parser.add_argument('--outfile', '--o', help='Output file path',
+                        required=True)
+    output_file = set_file_extension(parser.parse_args().outfile, 'xlsx')
     courses_count = abs(parser.parse_args().count)
-    courses_fetch_mode = parser.parse_args().mode
-
-    enable_win_unicode_console()
-
-    print('Получаем данные по %s курсам Coursera' % courses_count)
+    print('Fetching data on %s Coursera courses' % courses_count)
     courses_urls = get_courses_list(COURSERA_COURSES_XML)
-
     random.shuffle(courses_urls)
-    courses_urls = courses_urls[:courses_count]
-
     fetched_courses = []
-    for course_url in tqdm(courses_urls):
-        if courses_fetch_mode == 'api':
-            fetched_courses.append(get_course_info_from_api(course_url))
-        else:  # courses_fetch_mode == 'html'
-            fetched_courses.append(get_course_info(course_url))
-
-    try:
-        print('Выводим результаты в файл %s' % output_file)
-        output_courses_info_to_xlsx(fetched_courses, output_file)
-    except OSError as error:
-        print('Ошибка: %s в файле: %s' % (error.strerror, error.filename))
-        exit(1)
-
+    for course_url in tqdm(courses_urls[:courses_count]):
+        fetched_courses.append(get_course_info(course_url))
+    print('Saving to file %s' % output_file)
+    output_courses_info_to_xlsx(fetched_courses, output_file)
 
 if __name__ == '__main__':
-
     main()
